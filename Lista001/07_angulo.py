b0b68e114b8c3fea767d500d4f1ca1cfaa1380bc
vitoremanueldev <veslima3@gmail.com>
2020-11-29 01:39:18 -0300

    Lista finalizada

--- Lista001/07_angulo.py
+++ Lista001/07_angulo.py
@@ -4,9 +4,16 @@ from math import radians, sin, cos, tan
 angulo = 80
 
 seno = (sin(radians(angulo)))
-cosseno = (cos(radians(angulo)))
+co_seno = (cos(radians(angulo)))
 tangente = (tan(radians(angulo)))
+secante = 1 / (cos(radians(angulo)))
+co_secante = 1 / (sin(radians(angulo)))
+co_tangente = (cos(radians(angulo))) / (sin(radians(angulo)))
+
 
 print('{} seno'.format(round(seno,2)))
-print('{} cosseno'.format(round(cosseno, 2)))
+print('{} cosseno'.format(round(co_seno, 2)))
 print('{} tangente'.format(round(tangente, 2)))
+print('{} secante'.format(round(secante,2)))
+print('{} co_secante'.format(round(co_secante,2)))
+print('{} co_tangente'.format(round(co_tangente,2)))
--- Lista001/09_retangulo.py
+++ Lista001/09_retangulo.py
@@ -1,11 +1,10 @@
-import math
 
 Aa = 5
 Bb = 10
 
 area = Aa * Bb
 perimetro = Aa * 2 + Bb * 2
-diagonal = math.sqrt(Aa ** 2 + Bb ** 2)
+diagonal = (Aa ** 2 + Bb ** 2) ** (1/2)
 
 print('area = {}'.format(round(area, 2)))
 print('perimetro = {}'.format(round(perimetro, 2)))
