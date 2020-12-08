ec574c4fd099a969078c747f10fe30db9ae56b7c
vitoremanueldev <veslima3@gmail.com>
2020-12-08 09:56:41 -0300

    correções

--- Lista002/10_fatiamento.py
+++ Lista002/10_fatiamento.py
@@ -5,12 +5,12 @@ frase = "Python é muito legal"
 
 python = frase[0:6]
 eh = frase[7]
-muito = frase[8:14]
+muito = frase[9:14]
 legal = frase[-5:]
 
 
 print("Tamanho da frase é:", len(frase))
-print("Tamanho da de python é:", len(python))
-print("Tamanho da eh é:", len(eh))
-print("Tamanho da muito é:", len(muito))
-print("Tamanho da legal é:", len(legal))
+print("Tamanho de de", python, "é:", len(python))
+print("Tamanho de'", eh, "'é:", len(eh))
+print("Tamanho de'", muito, "'é:", len(muito))
+print("Tamanho de'", legal, "'é:", len(legal))
