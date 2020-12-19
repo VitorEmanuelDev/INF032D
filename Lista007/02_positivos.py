232641343e54e7b625d11b822a0bf33a367f1059
vitoremanueldev <veslima3@gmail.com>
2020-12-19 13:07:16 -0300

    Lista 07 exercicio 02

---
+++ Lista007/02_positivos.py
@@ -0,0 +1,11 @@
+#2.Entrar com números enquanto forem positivos, e imprimir quantos números foram
+#digitados;
+
+i = 0
+n = 1
+while n > 0:
+    n = int(input("Informe um número:"))
+    i += 1
+    if n <= 0:
+        print("Form digitados:", i,"números")
+        exit
