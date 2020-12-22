976bdc9f7e742b1f4e31ebf0d40848b9cbc54844
vitoremanueldev <veslima3@gmail.com>
2020-12-22 12:21:21 -0300

    Lista finalizada

--- Lista007/09_empresa.py
+++ Lista007/09_empresa.py
@@ -20,53 +20,69 @@ num_cons = 1
 i = 0
 
 while num_cons != 0:
+
     num_cons = int(input("Qual o número do consumidor?"))
     if num_cons == 0:
-        break
-    numero_consumidor.append(num_cons)
-    kWh_cons = float(input("Quantos kWh foram consumidos?"))
-    kWh_consumidor.append(kWh_cons)
-    print("1 - Residencial\n2 - Comercial\n3 - Industrial")
-    tipo_cons = int(input("Qual o tipo de consumidor?"))
-    tipo_consumidor.append(tipo_cons)
+        exit
+    else:
+        if num_cons < 0:
+            num_cons *= -1
+        numero_consumidor.append(num_cons)
 
-    if tipo_cons == 1:
-        custo.append(kWh_cons * .3)
-        cons_01 += 1
-    elif tipo_cons == 2:
-        custo.append(kWh_cons * .5)
-        cons_02 += 1
-    elif tipo_cons == 3:
-        custo.append(kWh_cons * .7)
-        cons_03 += 1
+        kWh_cons = float(input("Quantos kWh foram consumidos?"))
+        if kWh_cons < 0:
+            kWh_cons *= -1
+        kWh_consumidor.append(kWh_cons)
 
-    i += 1
+        print("1 - Residencial\n2 - Comercial\n3 - Industrial")
+        tipo_cons = int(input("Qual o tipo de consumidor?"))
+        if tipo_cons != 1 and tipo_cons != 2 and tipo_cons != 3 and tipo_cons != 0:
+            print("Escolha apenas uma das opções abaixo:")
+            print("1 - Residencial\n2 - Comercial\n3 - Industrial")
+            tipo_cons = int(input("Qual o tipo de consumidor?"))
+            if tipo_cons != 1 and tipo_cons != 2 and tipo_cons != 3 and tipo_cons != 0:
+                tipo_cons = 1
+                print("Tipo inválido. Atribuindo tipo 1.")
+        tipo_consumidor.append(tipo_cons)
 
-if num_cons == 0 and i == 0:
-    print("Reinicie o programa e tente novamente.")
+        if tipo_cons == 1:
+            custo.append(kWh_cons * .3)
+            cons_01 += 1
+        elif tipo_cons == 2:
+            custo.append(kWh_cons * .5)
+            cons_02 += 1
+        elif tipo_cons == 3:
+            custo.append(kWh_cons * .7)
+            cons_03 += 1
 
-n = i
-custo_total = sum(kWh_consumidor)
+        i += 1
 
-i = 0
-soma_kWh = 0
+if (num_cons == 0 and i == 0) or num_cons == 0:
+    print("Programa terminado.")
+    exit
+else:
+    n = i
+    custo_total = sum(kWh_consumidor)
 
-while i < n:
-    if tipo_consumidor[i] == 1 or tipo_consumidor[i] == 2:
-        soma_kWh += kWh_consumidor[i]
-    i += 1
+    i = 0
+    soma_kWh = 0
 
-media_01_02 = soma_kWh / (cons_01 + cons_02)
+    while i < n:
+        if tipo_consumidor[i] == 1 or tipo_consumidor[i] == 2:
+            soma_kWh += kWh_consumidor[i]
+        i += 1
 
-i = 0
+    media_01_02 = soma_kWh / (cons_01 + cons_02)
 
-while i < n:
-    print("\nNúmeros dos clientes:", numero_consumidor)
-    print("Consumo por cliente (kWh):", kWh_consumidor)
-    print("Tipo de cliente:", tipo_consumidor)
-    print("Preço/Custo total por consumidor (R$):", custo)
+    i = 0
 
-    i += 1
+    while i < n:
+        print("\nNúmeros dos clientes:", numero_consumidor)
+        print("Consumo por cliente (kWh):", kWh_consumidor)
+        print("Tipo de cliente:", tipo_consumidor)
+        print("Preço/Custo total por consumidor (R$):", custo)
 
-print("Consumo total (todos os tipos) (R$):", custo_total)
-print("Média de consumo (tipos 01 e 02) (kWh):", media_01_02)
+        i += 1
+
+    print("Consumo total (todos os tipos) (R$):", custo_total)
+    print("Média de consumo (tipos 01 e 02) (kWh):", media_01_02)
